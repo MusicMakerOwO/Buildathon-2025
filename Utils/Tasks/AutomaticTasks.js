1e8f02e38c9be238da2287efdf32b7e25c12308f
Music Maker <86620902+MusicMakerOwO@users.noreply.github.com>
2025-12-04 12:34:21 -0600

    INITIAL COMMIT

---
+++ Utils/Tasks/AutomaticTasks.js
@@ -0,0 +1,71 @@
+const { SECONDS } = require("../Constants");
+const Database = require("../Database");
+const { Log } = require("../Log");
+const TaskScheduler = require("../TaskScheduler");
+
+const TIME_BETWEEN_TASKS = SECONDS.MINUTE * 10; // 10 minutes
+
+const TASKS = [
+	// [ 'task_name', require("./TaskFile"), interval_in_seconds	]
+];
+
+for (let i = 0; i < TASKS.length; i++) {
+	TASKS[i][2] *= 1000; // convert interval to milliseconds
+}
+
+const longestName = Math.max( ... TASKS.map(t => t[0].length) );
+
+module.exports.StartTasks = async function StartTasks() {
+	const totalTasks = TASKS.length;
+	if (totalTasks === 0) {
+		Log('WARN', "No tasks to manage - nothing to do!");
+		return;
+	} else {
+		Log('SUCCESS', `Starting ${totalTasks} automatic tasks...`);
+	}
+
+	const selectQuery = Database.prepare("SELECT last_run FROM Timers WHERE id = ?");
+
+	for (let i = 0; i < TASKS.length; i++) {
+		const taskData = TASKS[i];
+		if (!Array.isArray(taskData) || taskData.length !== 3) {
+			Log('WARN', `Task entry ${i} is not an array, skipping...`);
+			continue;
+		}
+		const [ name, callback, interval ] = taskData;
+
+		if (typeof interval !== 'number' || interval <= 0 || !Number.isFinite(interval)) {
+			Log('WARN', `Task "${name}" does not have a defined interval, skipping...`);
+			continue;
+		}
+		if (typeof callback !== 'function') {
+			Log('WARN', `Task "${name}" does not have a callback function, skipping...`);
+			continue;
+		}
+		if (callback.constructor.name !== 'AsyncFunction') {
+			Log('WARN', `Task "${name}" callback must be an async function, skipping...`);
+			continue;
+		}
+
+		const lastRun = await selectQuery.pluck().get(name) || 0;
+		// rounding errors shouldn't be a concern because the exact second doesn't matter
+		const lastRunNumber = Number(lastRun);
+
+		const now = Date.now();
+		const timeSinceLastRun = Math.max(0, now - lastRunNumber);
+
+		const offset = (TIME_BETWEEN_TASKS * 1000) * i;
+		const delay = Math.max(timeSinceLastRun >= interval ? 0 : interval - timeSinceLastRun, offset);
+
+		TaskScheduler.schedule(() => {
+			try {
+				callback();
+				Database.prepare("INSERT INTO Timers (id, last_run) VALUES (?, ?) ON CONFLICT(id) DO UPDATE SET last_run = excluded.last_run").run(name, Date.now());
+			} catch (err) {
+				Log('ERROR', err);
+			}
+		}, delay, interval);
+
+		Log('SUCCESS', `[TASKS] - "${name}"${' '.repeat(longestName - name.length + 2)} : delayed ${(delay / 1000 / 60).toFixed(2)} minutes`);
+	}
+}
