99bf62d974b592986bddd07530560afeb32846f0
Music Maker <joshuanejdl@gmail.com>
2025-12-05 23:28:41 -0600

    feat: Even more obstacles to use

---
+++ Structures/GameObjects/Stove.js
@@ -0,0 +1,18 @@
+const StorageObstacle = require('../StorageObstacle');
+
+class Stove extends StorageObstacle {
+	/**
+	 * The stove is always unlocked, items are still optional.
+	 * @param {false} isLocked
+	 * @param {Item[]} contents
+	 */
+	constructor(isLocked, contents) {
+		isLocked = false;
+		super(
+			'Stove',
+			'An old-fashioned stove with a small oven compartment below the burners.',
+			isLocked,
+			contents
+		);
+	}
+}
