2e5ba91d9c84b9953526cb5cbcd69ae7a9011d4f
Music Maker <joshuanejdl@gmail.com>
2025-12-07 17:31:52 -0600

    chore: JSDoc for Obstacle class

--- Structures/Obstacle.js
+++ Structures/Obstacle.js
@@ -1,7 +1,12 @@
 // bad name, I know, but "object" is taken in JS
 class Obstacle {
+	/**
+	 * @param {string} name
+	 * @param {string} lore
+	 * @param {string[]} actions
+	 */
 	constructor(name, lore, actions) {
-		this.name = name;
+		this.name = String(name);
 		this.lore = String(lore || 'An immovable obstacle blocks your path.');
 
 		if (!Array.isArray(actions) || actions.some(x => typeof x !== 'string')) throw new Error('Actions must be an array of strings');
