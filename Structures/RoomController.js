170734d9a3c55cd5eec6b0e38e53a67f87f98a0b
Music Maker <joshuanejdl@gmail.com>
2025-12-07 13:28:41 -0600

    feat: Finished prototyping RoomController

--- Structures/RoomController.js
+++ Structures/RoomController.js
@@ -1,36 +1,59 @@
 const Item = require('./Item');
 const Obstacle = require('./Obstacle');
 const IsClass = require('../Utils/IsClass');
+const StorageObstacle = require('./StorageObstacle');
+const Key = require("./Items/Key");
 
 class RoomController {
 	/**
 	 * @param {string} description
-	 * @param {Obstacle[]} possibleObstacles - array of Obstacle classes to choose from
-	 * @param {Item[]} possibleItems - array of Item classes to choose from
-	 * @param {number} obstacleCount - how many obstacles to place in the room
-	 * @param {number} itemCount - how many items to place in the room
+	 * @param {StorageObstacle[]} storageObstacles
+	 * @param {Obstacle[]} obstacles
+	 * @param {Item[]} items
 	 */
-	constructor(description, possibleObstacles, possibleItems, obstacleCount, itemCount) {
+	constructor(description, storageObstacles, obstacles, items) {
 
-		for (const obstacleClass of possibleObstacles) {
+		for (const storageObstacleClass of storageObstacles) {
+			this.#VerifySubclass(storageObstacleClass, StorageObstacle);
+		}
+		for (const obstacleClass of obstacles) {
 			this.#VerifySubclass(obstacleClass, Obstacle);
 		}
-		for (const itemClass of possibleItems) {
+		for (const itemClass of items) {
 			this.#VerifySubclass(itemClass, Item);
+		}
+		if (storageObstacles.length === 0) {
+			throw new Error('At least one StorageObstacle subclass must be provided in obstacles array');
 		}
 
 		this.description = String(description);
 		this.items = new Map(); // Item class -> Item instance
 		this.obstacles = new Map(); // Obstacle class -> Obstacle instance
 
-		// Randomly populate automatically on creation
-		this.RandomizeContents(possibleObstacles, possibleItems, obstacleCount, itemCount);
+		this.#InitializeProps(storageObstacles, obstacles, items);
 	}
 
-	RandomizeContents(possibleObstacles, possibleItems, obstacleCount, itemCount) {
-		// TODO: Select random obstacles and items to populate the room
-		// Be careful that the room remains solvable!
-		// ie a locked door must have a key somewhere
+	#InitializeProps(storageObstacles, obstacles, items) {
+		// select 1 storage obstacle randomly to contain a key item
+		const randomIndex = Math.floor(Math.random() * storageObstacles.length);
+		const keyItem = new Key();
+		for (let i = 0; i < storageObstacles.length; i++) {
+			const StorageObject = i === randomIndex ? new storageObstacles[i]() : new storageObstacles[i](keyItem);
+			this.obstacles.set(storageObstacles[i], StorageObject); // class -> instance
+		}
+		for (const ObstacleClass of obstacles) {
+			const obstacleInstance = new ObstacleClass();
+			this.obstacles.set(ObstacleClass, obstacleInstance); // class -> instance
+		}
+		for (const ItemClass of items) {
+			const itemInstance = new ItemClass();
+			if (this.items.has(ItemClass)) {
+				const existingItem = this.items.get(ItemClass);
+				existingItem.count += itemInstance.count;
+			} else {
+				this.items.set(ItemClass, itemInstance); // class -> instance
+			}
+		}
 	}
 
 	#VerifySubclass(targetClass, baseClass) {
