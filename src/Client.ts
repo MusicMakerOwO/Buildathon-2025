6414f9bddc1f46c80c58e78d25cc7c955d31b41d
Music Maker <joshuanejdl@gmail.com>
2025-12-11 16:44:15 -0600

    feat: Added game cache structure to client

--- src/Client.ts
+++ src/Client.ts
@@ -1,6 +1,7 @@
 import { Client } from 'discord.js';
 import {ButtonHandler, CommandHandler, ModalHandler, SelectMenuHandler} from "./Typings/HandlerTypes";
 import config, {IConfig} from "./config";
+import {GameController} from "./Structures/GameController";
 
 interface IClient extends Client {
 	config: IConfig;
@@ -10,6 +11,21 @@ interface IClient extends Client {
 	menus: Map<string, SelectMenuHandler>;
 	modals: Map<string, ModalHandler>;
 	context: Map<string, CommandHandler>;
+
+	gameInstances: Map<number, {
+		instance: GameController;
+
+		// basic identifiers
+		guildID: string;
+		channelID: string;
+		userID: string;
+
+		// external metadata
+		/**
+		 * The number of logs to scroll up when displaying the game log
+		 */
+		logOffset: number;
+	}>; // Date.now() -> GameController instance
 }
 
 const client = new Client({
@@ -19,5 +35,6 @@ const client = new Client({
 }) as IClient;
 
 client.config = config;
+client.gameInstances = new Map();
 
 export { client, IClient }
