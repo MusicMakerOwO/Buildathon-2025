5f927bbdce96ff39460aa765b54ac92be3c55dc7
Music Maker <joshuanejdl@gmail.com>
2025-12-11 16:45:03 -0600

    fix: Updated InteractionHandler to be more "typescript friendly"

--- src/Events/InteractionHandler.ts
+++ src/Events/InteractionHandler.ts
@@ -5,12 +5,11 @@ import {Log} from "../Utils/Log";
 import {InteractionType} from "discord-api-types/v10";
 
 import config from '../config';
+import {ButtonHandler, CommandHandler, EventHandler, ModalHandler, SelectMenuHandler} from "../Typings/HandlerTypes";
 
-module.exports = {
+export default {
 	name: 'interactionCreate',
 	execute: async function (client: IClient, interaction: Interaction) {
-
-		const BoundHandler = InteractionHandler.bind(null, client, interaction);
 
 		switch (interaction.type) {
 			case 4: // Autocomplete
@@ -24,40 +23,40 @@ module.exports = {
 					const commandArgs: { value: string }[] = interaction.options._hoistedOptions || [];
 					const args = `${subcommandGroup} ${subcommand} ${commandArgs.map(arg => arg.value).join(" ")}`.trim();
 					Log('INFO', `${interaction.user.tag} (${interaction.user.id}) > /${interaction.commandName} ${args}`);
-					await BoundHandler('commands', client.commands);
+					InteractionHandler<CommandHandler>(client, interaction, 'commands', client.commands);
 				} else {
 					Log('INFO', `${interaction.user.tag} (${interaction.user.id}) > :${interaction.commandName}:`);
-					await BoundHandler('context', client.context);
+					InteractionHandler<CommandHandler>(client, interaction, 'context', client.context);
 				}
 				break;
 			case 3: // Message Components
 				if (interaction.isButton()) {
 					Log('INFO', `${interaction.user.tag} (${interaction.user.id}) > [${interaction.customId}]`);
-					await BoundHandler('buttons', client.buttons);
+					InteractionHandler<ButtonHandler>(client, interaction, 'buttons', client.buttons);
 				} else if (interaction.isAnySelectMenu()) {
 					Log('INFO', `${interaction.user.tag} (${interaction.user.id}) > <${interaction.customId}>`);
-					await BoundHandler('menus', client.menus);
+					InteractionHandler<SelectMenuHandler>(client, interaction, 'menus', client.menus);
 				}
 				break;
 			case 5: // Modal submit
 				Log('INFO', `${interaction.user.tag} (${interaction.user.id}) > {${interaction.customId}}`);
-				await BoundHandler('modals', client.modals);
+				InteractionHandler<ModalHandler>(client, interaction, 'modals', client.modals);
 				break;
 			default:
 				Log('ERROR', `Unknown interaction type: ${interaction.type} - Unsure how to handle this...`);
 				break;
 		}
 	}
-}
+} as EventHandler;
 
-async function InteractionHandler(client: IClient, interaction: Interaction, type: string, cache: Map<string, any>) {
+async function InteractionHandler<HandlerType extends CommandHandler | ButtonHandler | ModalHandler | SelectMenuHandler>(client: IClient, interaction: Interaction, type: string, cache: Map<string, any>) {
 
 	// @ts-ignore
 	const args: string[] = interaction.customId?.split("_") ?? [];
 	// @ts-ignore
 	const name: string = args.shift() ?? interaction.commandName;
 
-	const component = cache.get(name);
+	const component = cache.get(name) as HandlerType | undefined;
 	if (!component) {
 		if (interaction.type === InteractionType.ApplicationCommandAutocomplete) {
 			throw new Error('Command not found');
@@ -71,8 +70,9 @@ async function InteractionHandler(client: IClient, interaction: Interaction, typ
 	}
 
 	try {
-		const callback = interaction.isAutocomplete() ? component.autocomplete : component.execute;
+		const callback = interaction.isAutocomplete() ? (component as CommandHandler).autocomplete : component.execute;
 		if (typeof callback !== 'function') throw new Error('Command not implemented');
+		// @ts-ignore - Complaining that Interaction is not assignable to more specific types, but it really does not matter in this case lmao
 		await callback(interaction, client, type === 'commands' ? undefined : args);
 	} catch (error) {
 		Log('ERROR', error);
