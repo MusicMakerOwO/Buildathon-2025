acc74904c9f1b12d3240d2839da0cfc5511eff6e
Music Maker <joshuanejdl@gmail.com>
2025-12-13 15:54:51 -0600

    feat: Additional obstacles for rooms

---
+++ src/Structures/GameObjects/Bench.ts
@@ -0,0 +1,10 @@
+import {Obstacle} from "../Obstacle";
+
+export class Bench extends Obstacle {
+	constructor() {
+		super(
+			'Bench',
+			'A sturdy wooden bench, worn smooth from years of use.',
+		);
+	}
+}
---
+++ src/Structures/GameObjects/Table.ts
@@ -0,0 +1,10 @@
+import {Obstacle} from "../Obstacle";
+
+export class Table extends Obstacle {
+	constructor() {
+		super(
+			'Table',
+			'A sturdy wooden table, scarred from years of use. It looks like it could support a fair amount of weight.',
+		);
+	}
+}
