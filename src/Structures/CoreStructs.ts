a6afb5ba53149478f02e9b1922fc65aa4a68b16f
Music Maker <joshuanejdl@gmail.com>
2025-12-14 19:55:55 -0600

    feat: Added an entity class for representing characters and basic combat

--- src/Structures/CoreStructs.ts
+++ src/Structures/CoreStructs.ts
@@ -1,6 +1,51 @@
 import {Log} from '../Utils/Log';
 import {PlayerController} from "./PlayerController";
 
+export class Entity {
+
+	name: string;
+	description: string;
+	max_health: number;
+	current_health: number;
+	damage: {min: number, max: number} | number;
+
+	constructor(name: string, description: string, health: number, damage: {min: number, max: number} | number) {
+		this.name = name;
+		this.description = description;
+		this.max_health = this.current_health = health;
+		this.damage = damage;
+	}
+
+	getDamage(): number {
+		if (typeof this.damage === 'number') {
+			return this.damage;
+		} else {
+			const min = this.damage.min;
+			const max = this.damage.max;
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		}
+	}
+
+	get isAlive(): boolean {
+		return this.current_health > 0;
+	}
+
+	hurt(amount: number): void {
+		this.current_health -= amount;
+		if (this.current_health < 0) {
+			this.current_health = 0;
+		}
+	}
+
+	heal(amount: number, capHealth = true): void {
+		const prevHealth = this.current_health;
+		this.current_health += amount;
+		if (capHealth && this.current_health > this.max_health) {
+			this.current_health = prevHealth;
+		}
+	}
+}
+
 // bad name, I know, but "object" is taken in JS
 export class Obstacle {
 
