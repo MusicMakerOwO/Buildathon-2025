86b092eb11d1886966276a8f902d0042b114ad46
Music Maker <joshuanejdl@gmail.com>
2025-12-14 21:01:02 -0600

    refactor: Adjusted imports for items

--- src/Structures/Items/Key.ts
+++ src/Structures/Items/Key.ts
@@ -1,4 +1,4 @@
-import {Item} from '../Item.js';
+import {Item} from '../CoreStructs';
 
 export class Key extends Item {
 	constructor() {
--- src/Structures/Items/Rock.ts
+++ src/Structures/Items/Rock.ts
@@ -1,4 +1,4 @@
-import {Item} from "../Item";
+import {Item} from "../CoreStructs";
 
 export class Rock extends Item {
 	constructor() {
