006bc87fead5cc205038865103ab759962bdbc14
Music Maker <joshuanejdl@gmail.com>
2025-12-16 01:43:43 -0600

    feat: Converted test to use ts for the benefit of intellisense and added type safety

--- src/Tests/PropContents.test.ts
+++ src/Tests/PropContents.test.ts
@@ -1,11 +1,12 @@
-const props = require('../build/Structures/GameObjects.js');
-const {Key} = require("../build/Structures/Items.js");
+import {test, expect} from "@jest/globals";
+import {Key} from "../Structures/Items";
+import * as props from "../Structures/GameObjects";
 
 const key = new Key();
 
 for (const prop of Object.values(props)) {
 	test(`${prop.name} has functional inventory`, () => {
-		const propInstance = new prop(key);
+		const propInstance = new prop(false);
 		propInstance.addItem(key);
 		expect(propInstance.contents[0]).toBe(key);
 	});
